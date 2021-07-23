4bbf6ffc88bdc8cbc159df0d6847baa8aec53555
Chris du Plessis <christopherjonduplessis@gmail.com>
2021-07-23 19:40:06 +0200

    Test commit

--- main.jl
+++ main.jl
@@ -11,3 +11,4 @@ include("post_processing.jl")
 settings = Settings(1024, 1024, -2, 2, 2, -2, 100, 2, 0, (zn, c) -> (zn)^2 + c, identity, identity, :buddha, (512, 512), false, false, Float32)
 img = create_image(settings)
 imshow(colour(img, settings))
+
